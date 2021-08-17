f4de3c62d8811ca379a9296e0cce001d42dfc8bb
Hamedfuladi <89084607+hamedfuladi@users.noreply.github.com>
2021-08-17 19:59:12 +0430

    Update persian-number.js

--- persian-number.js
+++ persian-number.js
@@ -1,23 +1,56 @@
 /**
- * Convert latin numbers in a string to arabic/persian number
- * @param {sting} sting - The string that you wanna convert
- * @return {string} - Converted string with 
+ * Convert latin numbers in a string to persian number
+ * @param {srting} srting - The string that you wanna convert
+ * @return {string} - Converted string with persian digits
  */
 
-function toPersianDigits(sting) {
-  var nums = [
-    '۰',
-    '۱',
-    '۲',
-    '۳',
-    '۴',
-    '۵',
-    '۶',
-    '۷',
-    '۸',
-    '۹'
-  ];
-  return sting.replace(/[0-9]/g, function (w) {
-    return nums[ + w]
-  })
+function toPersianDigits(srting) {
+  
+  if(typeof string === 'string')
+  {
+    var nums = [
+      '۰',
+      '۱',
+      '۲',
+      '۳',
+      '۴',
+      '۵',
+      '۶',
+      '۷',
+      '۸',
+      '۹'
+    ];
+    string =  string.replace(/[0-9]/g, function (w) {
+      return nums[ + w]
+    })
+  }
+  return string
+}
+
+/**
+ * Convert persian numbers in a string to lating number
+ * @param {srting} srting - The string that you wanna convert
+ * @return {string} - Converted string with lating digits
+ */
+
+function persiantoLatinDigits(string){
+var persianNums = [/۰/g,
+                  /۱/g,
+                  /۲/g,
+                  /۳/g,
+                  /۴/g,
+                  /۵/g,
+                  /۶/g,
+                  /۷/g,
+                  /۸/g,
+                  /۹/g
+                 ];
+  if(typeof string === 'string')
+  {
+    for(var i=0; i<10; i++)
+    {
+      string = string.replace(persianNums[i], i);
+    }
+  }
+  return string;
 }
