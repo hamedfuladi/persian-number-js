9021cb598166532089cffd6f9cd3a2800ce53ee7
Hamedfuladi <89084607+hamedfuladi@users.noreply.github.com>
2023-01-20 22:54:00 +0330

    updating code
    updating the code and adding some error handling

--- persian-number.js
+++ persian-number.js
@@ -1,56 +1,42 @@
 /**
  * Convert latin numbers in a string to persian number
- * @param {srting} srting - The string that you wanna convert
+ * @param {string} string - The string that you want to convert
  * @return {string} - Converted string with persian digits
  */
-
-function toPersianDigits(srting) {
-  
-  if(typeof string === 'string')
-  {
-    var nums = [
-      '۰',
-      '۱',
-      '۲',
-      '۳',
-      '۴',
-      '۵',
-      '۶',
-      '۷',
-      '۸',
-      '۹'
-    ];
-    string =  string.replace(/[0-9]/g, function (w) {
-      return nums[ + w]
-    })
+function toPersianDigits(string) {
+  if(typeof string !== 'string'){
+    return "Input should be a string";
   }
-  return string
+  const nums = [
+    '۰',
+    '۱',
+    '۲',
+    '۳',
+    '۴',
+    '۵',
+    '۶',
+    '۷',
+    '۸',
+    '۹'
+  ];
+  string =  string.replace(/[0-9]/g, function (w) {
+    return nums[ + w]
+  });
+  return string;
 }
 
 /**
- * Convert persian numbers in a string to lating number
- * @param {srting} srting - The string that you wanna convert
- * @return {string} - Converted string with lating digits
+ * Convert persian numbers in a string to latin number
+ * @param {string} string - The string that you want to convert
+ * @return {string} - Converted string with latin digits
  */
-
 function persiantoLatinDigits(string){
-var persianNums = [/۰/g,
-                  /۱/g,
-                  /۲/g,
-                  /۳/g,
-                  /۴/g,
-                  /۵/g,
-                  /۶/g,
-                  /۷/g,
-                  /۸/g,
-                  /۹/g
-                 ];
-  if(typeof string === 'string')
-  {
-    for(var i=0; i<10; i++)
-    {
-      string = string.replace(persianNums[i], i);
-    }
+  if(typeof string !== 'string'){
+    return "Input should be a string";
+  }
+  const persianNums = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
+  for(let i = 0; i < 10; i++){
+    string = string.replace(persianNums[i], i);
   }
   return string;
 }
